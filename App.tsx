53373a676940015e7b52c8f04c7fe9dafaa29a0d
Mohamed Baher <mohamed.baher@valify.me>
2024-10-03 12:28:07 +0300

    App.tsx

--- App.tsx
+++ App.tsx
@@ -12,12 +12,12 @@ import { startLiveness } from '@valifysolutions/react-native-vidvliveness';
 
 // Unified credentials
 const creds = {
-  baseURL: 'https://www.valifystage.com/', // Update with your actual base URL
-  bundleKey: 'ad44eb94ca6747beaf99eef02407221f',
-  userName: 'mobileusername', // Replace with actual credentials
-  password: 'q5YT54wuJ2#mbanR',
-  clientID: 'aKM21T4hXpgHFsgNJNTKFpaq4fFpoQvuBsNWuZoQ',
-  clientSecret: 'r0tLrtxTue8c4kNmPVgaAFNGSeCWvL4oOZfBnVXoQe2Ffp5rscXXAAhX50BaZEll8ZRtr2BlgD3Nk6QLOPGtjbGXYoCBL9Fn7QCu5CsMlRKDbtwSnUAfKEG30cIv8tdW',
+  baseURL: '', // Update with your actual base URL
+  bundleKey: '',
+  userName: '', // Replace with actual credentials
+  password: '',
+  clientID: '',
+  clientSecret: '',
 };
 
 const getToken = async () => {
